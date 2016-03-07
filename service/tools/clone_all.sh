eabf638158dcdce53cd89262ae621f3f665fb460
Xiaolong Han <kpkhxlgy0@163.com>
2016-03-07 17:56:26 +0800

    x

---
+++ service/tools/clone_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+#clone all the gonet2 repo into current dir.
+mkdir gonet2
+cd gonet2
+
+git clone git@github.com:gonet2/agent.git
+git clone git@github.com:gonet2/game.git
+git clone git@github.com:gonet2/snowflake.git
+git clone git@github.com:gonet2/chat.git
+git clone git@github.com:gonet2/rank.git
+git clone git@github.com:gonet2/geoip.git
+git clone git@github.com:gonet2/archiver.git
+git clone git@github.com:gonet2/wordfilter.git
+git clone git@github.com:gonet2/tools.git
+
